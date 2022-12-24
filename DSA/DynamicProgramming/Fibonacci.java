ae4ef9fd0d45c4c0e45f5196cdd3780069553a9d
Amit Pawar <amitpawar7475@gmail.com>
2022-12-24 23:08:22 +0530

    kd

--- DSA/DynamicProgramming/Fibonacci.java
+++ DSA/DynamicProgramming/Fibonacci.java
@@ -1,17 +1,22 @@
 package DSA.DynamicProgramming;
 
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
         int num = 5;
 
         int[] dp = new int[num + 1];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = -1;
-        }
+        // for (int i = 0; i < dp.length; i++) {
+        //     dp[i] = -1;
+        // }
+
+        Arrays.fill(dp, -1);
 
         System.out.println(fibonacci(num, dp));
     }
 
+    // Memoization
     public static int fibonacci(int num, int[] dp) {
         if (num <= 1) {
             return num;
